6a7a02ea8556058fd0ce395c02bdef987915cf2b
Logos <132839177+logoskal@users.noreply.github.com>
2024-12-15 22:03:00 +0300

    First Commit

---
+++ thechurch/home/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+import home.views as views
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = [
+    path('', views.home, name='home-page'),
+    path('welcome/', views.home, name='welcome-page'),
+    path('about/', views.about, name='welcome-page'),
+    path('socials/', views.socials, name='socials-page'),
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ thechurch/home/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+location = 'https://maps.app.goo.gl/QQibjhPHRzZNoBT88'
+
+def home(request):
+    return render(request, template_name='home/home.html')
+
+def socials(request):
+    return render(request, template_name='home/socials.html')
+
+def about(request):
+    return render(request, template_name='home/about.html')
+
+def socials(request):
+    return render(request, template_name='home/socials.html')
+
+def socials(request):
+    return render(request, template_name='home/socials.html')
